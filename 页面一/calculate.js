6b1c4be670163fed88ff1820788c452a3bb04705
326 <2405825227@qq.com>
2021-08-10 22:04:35 +0800

    1

--- 页面一/calculate.js
+++ 页面一/calculate.js
@@ -455,3 +455,592 @@ var isPalindrome = function(x) {
 
 
 
+
+//matrix里面寻找给定的 数据是否存在
+//从第一行最后一个数据是转行的节点位置
+//算法的节点关键位置找准之后就轻而易举
+//用变量数据的时候先考虑数据是否存在可用
+//这道题目的关键节点就是每一行最末尾数据        决定了下一轮循环是前往下面一行数据      还是前往前面的数据内容
+var findNumberIn2DArray = function(matrix, target) {
+    if(matrix.length == 0){
+     return false
+         }
+         //若是没有第一个判断语句  而直接就是用matrix[0].length 那么遇到空的matrix的时候就会报错    程序不可继续执行下去
+    let n = 0;m = matrix[0].length - 1;
+    //数据长度范围的限制
+    while(n < matrix.length && m >= 0){
+        if(target == matrix[n][m]){
+            return true
+        }else{
+            if(target > matrix[n][m]){
+                //前往下面一行
+                n++
+            }else{
+                //前往上一个数据
+                m--
+            }
+        }
+        
+        
+    }
+    
+   return false
+};
+
+
+
+
+
+
+
+//第一个只出现一次的字符
+var firstUniqChar = function(s) {
+    //遍历数据得出第一次出现和最后一次出现的下标是一样的
+    for(let x of s){
+        if(s.indexOf(x) === s.lastIndexOf(x)) return x
+    }
+    return ' '
+};
+
+
+
+
+//自己写数组去重        indexOf()  push
+var unique = function(arr1){
+    let arr2 = []
+    let len = arr1.length
+    for(let i = 0;i < len;i++){
+        if(arr2.indexOf(arr1[i]) == -1){
+            arr2.push(arr[i])
+        }
+    }
+    return arr2
+}
+
+
+//数组装水问题就是  两边向中心汇聚  来计算最大的值
+var res = function(arr){
+    let len = arr.length
+    let R = len - 1,L = 0
+    let res = 0
+    while(R>L){
+        let minNum = Math.min(arr[R],arr[L])
+        res = Math.max(minNum*(R - L),res)
+        if(arr[R]>arr[L]){
+            L++
+        }else{
+            R--
+        }
+    }
+}
+
+
+
+//整数转换位罗马数字
+// M 1000 CM 900  D 500 CD 400 C 100 
+var intToRoman = function(num) {
+    //二维数组形成了一对一的关系        那么就是value    和   symbol的关系
+    const valueSymbols = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]];
+    const roman = [];
+    for (const [value, symbol] of valueSymbols) {
+        //从大到小依次来循环进行输出
+        while (num >= value) {
+            //满足一个整数的标志
+            num -= value;
+            //那么就插入一个罗马字符串
+            roman.push(symbol);
+        }
+        //直到数据为 0 的时候就推出循环
+        if (num == 0) {
+            break;
+        }
+    }
+    //最后连接字符串
+    return roman.join('');
+};
+
+
+
+
+//罗马数字转换为了整数数字
+//创新用法  用到了map函数       用到了get   set函数     set(key,value)
+var romanToInt = function(s){
+    var mySet = new Map()
+    mySet.set('I', 1);
+    mySet.set('V', 5);
+    mySet.set('X', 10);
+    mySet.set('L', 50);
+    mySet.set('C', 100);
+    mySet.set('D', 500);
+    mySet.set('M', 1000);  
+    let arr = 0
+    let len = s.length
+    for(let i = 0;i < len;i++)
+    {
+        //get()获取属性值
+        var value = mySet.get(s[i])
+        if(i<len-1&&value<mySet.get(s[i+1])){
+            //罗马数字的特点就是前面的字母小于后面的字母的时候      数字的大小就减去前面的数字  然后就获取了数据
+            arr -= value
+        }else{
+            //前面字母不小于后面字母的时候      直接加上前面字母的大小对应值
+            arr += value
+        }
+    }
+    //最后返回结果值
+return arr
+}
+
+
+
+
+
+//寻找最长的相同的字符串前缀        然后就是输出while()循环     条件最好添加在最后面    不然会出现纰漏
+var longestCommonPrefix = function(strs) {
+    let len = strs.length
+let minLen = strs[0].length
+let str1 = ''
+//在数组里面的字符串找出长度最小的字符串长度        Math.min()      s.length
+for(const s of strs){
+    minLen  = Math.min(minLen,s.length)
+}
+while(minLen){
+str1 = strs[0].slice(0,minLen)
+
+for(let i = 0;i < len;i++){
+    if(strs[i].slice(0,minLen) == str1){
+        if(i == len - 1){
+            //得出结论的条件就是        最后一个字符串满足前缀相同
+            return str1
+        }
+        continue
+    }else{
+        break
+    }
+
+}
+minLen--
+}
+return ''
+
+};
+
+
+
+
+//三个数的和        循环的时候忘记了数组去重的操作  有三处操作时数组去重
+var threeSum = function(nums) {
+    let len = nums.length
+    let arr1 = []
+    let arr2 = []
+    //
+    if(len < 3)return arr1
+    //数组排序
+    nums.sort((a,b)=>a-b)
+    if(nums[0]>0 || nums[len-1]<0)return arr1
+    let R = len - 1,L = 0
+    for(let i = 0;i < len;i++){
+        if(i>0&&nums[i]==nums[i-1])continue//去重   第一个数字相同  那么后面找到的数字就是  也会相同    避免重复就要去重操作
+        L = i + 1
+        R = len - 1
+        while(L < R){
+        if(nums[i] + nums[L] + nums[R] == 0)
+        {
+            arr2.push([nums[i],nums[L],nums[R]])
+            while(L<R&&nums[L]==nums[L+1])L++//去重         同上
+            while(L<R&&nums[R]==nums[R-1])R--//去重
+            L++
+            R--
+        }
+        else if(nums[i] + nums[L] + nums[R] > 0)
+        {
+            R--
+        }else{
+            L++
+        }
+
+        }
+    }
+    return arr2
+}
+
+
+//三个数之和最接近target的结果
+var threeSumClosest = function(nums, target) {
+    nums.sort((a,b)=>a-b)
+    let L = 0,R = 0
+    let len = nums.length
+    let minNum = Infinity
+    let res = 0
+    for(let i = 0;i < len;i++){
+        R = len - 1
+        L = i + 1
+        while(L<R){
+            const sum = nums[i]+nums[L]+nums[R]
+            const minNum1 = Math.abs(sum-target)
+
+            if(minNum1<minNum){
+                //和数和目标数相差更加小的时候就会赋值
+                minNum = minNum1
+                res = sum
+            }
+            //和数大于目标数    R--
+            if(sum > target){
+                R--
+            }
+            else{
+                L++
+            }
+        }
+    }
+    return res
+    };
+
+
+
+//
+
+    var letterCombinations = function (digits) {
+        let arr = [];
+        //对象存储属性值        数字对应的字母      {[]}
+        const obj = {
+            2: ["a", "b", "c"],
+            3: ["d", "e", "f"],
+            4: ["g", "h", "i"],
+            5: ["j", "k", "l"],
+            6: ["m", "n", "o"],
+            7: ["p", "q", "r", "s"],
+            8: ["t", "u", "v"],
+            9: ["w", "x", "y", "z"],
+        };
+        //拆分输入的数据    放进arr数组存放     遍历数组的时候拿出来        意义比较循环输出结果
+        let digits2arr = digits.split("");
+        for (let i = 0; i < digits2arr.length; i++) {
+            //第一轮 遍历开始
+            let temp = obj[digits2arr[i]];
+            //起始数据的装载        进入
+            if (arr.length == 0) {
+                //三点运算符的应用
+                arr.push(...temp);
+            } else {
+                //进入else  代表就是进入了第二个数据的读入以及遍历
+                let medium = [];
+                for (const item of arr) {
+                    //数据的读入操作始终是以第一个数据为开始来读取的
+                    for (const item1 of temp) {
+                        //字符串的添加读取操作
+                        medium.push(item + item1);
+                    }
+                }
+                //for循环
+                arr = newArr;
+            }
+        }
+        return arr;
+    };
+
+
+var res = function(digits){
+    let arr = []
+    const obj = {
+        2: ["a", "b", "c"],
+        3: ["d", "e", "f"],
+        4: ["g", "h", "i"],
+        5: ["j", "k", "l"],
+        6: ["m", "n", "o"],
+        7: ["p", "q", "r", "s"],
+        8: ["t", "u", "v"],
+        9: ["w", "x", "y", "z"],
+    };
+    for(let i = 0;i < digits.length;i++){
+        const temp = obj[digits[i]]
+        //第一轮循环开始
+        if(!arr.length){
+            arr.push(...temp)
+        }else{
+            let medium = []
+            for( const c1 of arr){
+                for(const c2 of temp){
+                    medium.push(c1+c2)
+                }
+            }
+            arr = medium
+        }
+    }
+    return arr
+}
+
+
+
+//给定了二叉树的结构 [3,9,20,null,null,15,7]           输出没有null的完整结构二叉树 [3,9,20,15,7]
+var res = function(root){//root是二叉树的头节点     仅仅只有头节点  不包含后续节点的属性值
+    //返回值不是null        而是空数组[]
+if(!root)return []
+//二叉树转换为了队列进行运算
+const queue = [root]
+//装载新的二叉树的数组存储
+let arr2 = []
+while(queue.length){
+    const medium = queue.shift()
+    arr2.push(medium.val)
+    //根据位运算符号&&的特点就是      前面运算为假的时候直接结束了运算  那么相当于  if(){}简写了代码块
+    medium.left&&queue.push(medium.left)
+    medium.right&&queue.push(medium.right)
+}
+return arr2
+}
+
+
+
+
+//二叉树的层序遍历
+var  levelOrder = function(root){
+    //根节点是空的  直接返回空数组
+    if(!root){
+        return []
+    }
+    let arr1 = []
+    let queue = [root]
+    //遍历二叉树节点的开始
+    while(queue.length){
+        let arr2 = []
+        //因为queue的长度经过一轮循环之后   值是不确定变化的    每一轮的长度就是这一层数的节点的个数
+    for(let i = queue.length;i > 0;i--){
+        let n = queue.shift()
+        arr2.push(n.val)
+        //节点的左右节点是否插入的问题
+        n.left&&queue.push(n.left)
+        n.right&&queue.push(n.right)
+
+    }
+    arr1.push(arr2)
+}
+return arr1
+}
+
+
+
+
+
+//链表标记删除元素      删除的是倒数的元素
+//链表的结构就是        val  next结构
+var removeNthFromEnd  = function(head,n){
+    //定义要返回的链表的值属性
+    let headList = new ListNode(0,head)
+    //前后指针的定义
+    slow = fast = headList
+    //需要删除的元素的个数      fast后面移动n个元素
+    while(n--)fast = fast.next
+    //fast移动到了最后一个数据      防止移动到了最后一个数据
+    if(!fast)return headList.next
+    //假设链表的总长度是        N   fast移动了n     剩下了N-n =m      继续移动N-n=m     
+    //利用数据的互补的性质就是      m下一个对应的就是要删除的元素
+    while(fast.next){
+        fast = fast.next
+        slow = slow.next
+
+    }
+
+    //自己思考的逻辑是
+    // while(fast){
+    //     fast = fast.next
+    //     slow = slow.next
+    // }
+
+    slow.next = slow.next.next
+    return headList.next
+
+}
+
+
+
+
+
+//判断回文字符串是不是正确的
+var isValid = function(s) {
+    //定义一个栈队列        来进入栈  
+    var stack = []
+    //回文字符串的 长度是奇数的时候直接返回错误
+    if(s.length % 2){
+        //返回错误
+        return false
+    }
+    for(var i = 0;i < s.length;i++){
+        if(s[i] == '('){
+            //第一个进入栈队列      遍历的时候就是第一个出去栈队列     就是完全回文的操作
+        stack.push(')')}
+        else if(s[i] == '{'){
+        stack.push('}')}
+        else if(s[i] == '['){
+        stack.push(']')}else{
+            if(stack.pop()!=s[i])
+            return false
+        }
+    
+    }
+    //最后栈遍历完成        长度应该是0
+    return stack.length == 0 
+    };
+
+
+
+
+//链表的 应用       链表1和链表2的排序问题
+var mergeTwoLists = function(l1, l2) {
+//新创建的链表      new ListNode(-1)
+    var newList1 = new ListNode(-1)
+    //链表的赋值引用        这样newList1就永远指向了头节点的值      但是总体的链表数据改变了
+    let newList = newList1
+    //链表都不可以为空链表
+while(l1!=null && l2!=null)
+{
+   if(l1.val >= l2.val)
+   {
+       //链表交换数据       交换数据的时候是交换的指向数据方向的问题
+       newList.next = l2
+       //链表的指向已经被使用了     那么接下来就是链表下一个数据
+       l2 = l2.next 
+   }else
+   {
+       newList.next = l1
+       l1 = l1.next 
+   }
+   //傀儡链表指向下一个数据
+   newList = newList.next
+
+}
+
+//最后的数据体现        就是l1和l2中    有一个为空的时候        傀儡链表直接就指向了不为空的数据
+newList.next = l1==null?l2:l1
+//newList1始终指向的是新链表头节点的数据指向问题        那么最后返回数据
+return newList1.next
+};
+
+
+
+
+//不重复括号        n对括号函数     不重复括号输出
+var generateParenthesis = function(n) {
+    const result = [];
+    //n==0代表没有括号可以输出      那么最后直接输出空数组[]
+    if (n == 0) {
+        return result;
+    }
+    //Node()函数就是        节点的值,相关数据1,相关数据2
+    const queue = [new Node('', n, n)];
+    //n对括号说明有2n个数据的输入输出
+    n = 2 * n;
+    while (n > 0) {
+        //队列的长度
+        const size = queue.length;
+        for (let i = 0; i < size; i++) {
+            //取出队列的头节点
+            const curNode = queue.shift();
+            if (curNode.left > 0) {
+                //第一个数据        也就是说左边的括号还有剩余的 情况       push操作就会插入一个左边括号数据
+                queue.push(new Node(curNode.res + "(", curNode.left - 1, curNode.right));
+            }
+            if (curNode.right > 0 && curNode.left < curNode.right) {
+                //上同
+                queue.push(new Node(curNode.res + ")", curNode.left, curNode.right - 1));
+            }
+        }
+        //不明白为什么只除去了一个括号数据
+        n--;
+    }
+
+    while (queue.length) {
+        //取出queue队列里面的数据括号对应值     放入result数组输出
+        result.push(queue.shift().res);
+    }
+    return result;
+};
+
+
+
+
+
+var generateParenthesis = function (n) {
+    const res = [];
+  
+    //LRemain   rRemain是一种状态       广度优先算法就是不断消耗状态规律    直到状态消耗完成    是一种状态消除的递归算法
+    const dfs = (lRemain, rRemain, str) => { // 左右括号所剩的数量，str是当前构建的字符串
+      if (str.length == 2 * n) { // 字符串构建完成
+        //一轮递归完成之后产生的不重复的数据    加入到对应的字符数集
+        res.push(str);           // 加入解集
+        return;                  // 结束当前递归分支
+      }
+      //要求左边状态括号数据还存在
+      if (lRemain > 0) {         // 只要左括号有剩，就可以选它，然后继续做选择（递归）    
+        //进入左边状态数据存在的递归调用情况
+        dfs(lRemain - 1, rRemain, str + "(");
+      }
+      //必须有左边状态数据的消耗    右边状态数据才能允许消耗    递归
+      if (lRemain < rRemain) {   // 右括号比左括号剩的多，才能选右括号
+        dfs(lRemain, rRemain - 1, str + ")"); // 然后继续做选择（递归）
+      }
+    };
+  
+    dfs(n, n, ""); // 递归的入口，剩余数量都是n，初始字符串是空串
+    return res;
+  };
+
+
+
+
+  var mergeKLists = function (lists) {
+    const res = temp = new ListNode(null)
+    //pre是计算结束之后的返回值     cur是当前元素
+    lists.reduce((pre, cur) => {
+        while (cur) {
+            //链表节点插入pre数组里面
+            pre.push(cur)
+            //链表节点指向下一个节点
+            cur = cur.next
+        }
+        return pre
+    }, []).sort((n1, n2) => n1.val - n2.val).forEach(item => {
+        //sort()排序链表的节点数据
+        temp.next = new ListNode(item.val)
+        console.log(temp)
+        temp = temp.next
+    })
+    return res.next
+};
+
+
+
+//链表的两两交换的问题就是
+//用到了递归        递归是 两两数据递归     规定数据的长度是大于2的 
+//链表的长度遍历时  head.next               
+var swapPairs = function(head) {
+    //交换问题涉及到的数据是两个以上的数据      那么就是长度小于2的时候 就直接返回原来的连表示数据
+    if(head == null || head.next == null){
+        return head;
+    }
+    //定义一个链表节点指向  当前准备交换的数据变量      保存第二个数据的索引        准备交换数据值的时候        避免数据值丢失的尴尬
+    //定义了头节点的索引保存
+    const newHead = head.next
+    //swapPairs()代表的就是下一轮数据的循环     但是前面要求循环的长度必须大于2
+    head.next = swapPairs(newHead.next)
+    //交换数据
+    newHead.next = head
+    //由于前面已经定义了头节点的索引数据     那么就可以安心使用完成递归之后     返回头节点的索引
+    return newHead
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
